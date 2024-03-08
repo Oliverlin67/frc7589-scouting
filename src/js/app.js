d0c552143eb2a9e27689fc3e6537093a0ce60a3a
Paul-user <u11232107@lssh.tp.edu.tw>
2024-03-08 20:37:17 +0800

    發現一個大小寫錯誤

--- src/js/app.js
+++ src/js/app.js
@@ -259,7 +259,7 @@ window.getRate = (data) => {
                 formula = formula.replaceAll(parameter.alias, data[parameter.alias] ? 1 : 0);
             } else if(data[parameter.alias] !== undefined) {
                     formula = formula.replaceAll(parameter.alias, data[parameter.alias]);
-            } else if(parameter.alias.includes("Attempt") && (parameter.alias.includes("Auto") || parameter.alias.includes("Teleop")) && data[parameter.alias] == 0) {
+            } else if(parameter.alias.includes("Attempt") && (parameter.alias.includes("auto") || parameter.alias.includes("teleop")) && data[parameter.alias] == 0) {
                 if(parameter.alias.includes("Attempt")) {
                     formula = formula.replaceAll(parameter.alias, "1");
                 }
